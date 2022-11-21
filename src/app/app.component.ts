9921559488fa7cbcc99b348757d45e975e93fbb5
sinithaCB <sinitha@ttct.net>
2022-11-21 11:01:31 +0530

    intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculator';
+  InputValue = '0';
+  currVal = '';
+  operate( value: string){
+    this.currVal = this.currVal +  value;
+    this.InputValue = this.currVal;
+ }
+ Result(){
+    this.InputValue = eval(this.InputValue);
+    
+ }
+ Clear(){
+  this.currVal = '';
+  this.InputValue = '0';
+ }
 }
